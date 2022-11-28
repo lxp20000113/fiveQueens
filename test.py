d27d7733e38480f82140879c558fb01ffdae74e0
lilongqian <812386917@qq.com>
2022-11-28 15:44:53 +0800

    solve the problem

--- test.py
+++ test.py
@@ -6,27 +6,25 @@ s = Solver()
 我们这里需要创建一个[5][5]的矩阵，表明该位置上是否有皇后，当为1时，证明有皇后；为0时，证明没有皇后。
 '''
 
-n = 5
-Isempress = [[] for i in range(n)]
-for i in range(n):
-    for j in range(n):
-        Isempress[i].append(Bool(str(i) + "-" + str(j)))
+n = 4
+Isempress = [[Int("x_%s_%s" % (i, j)) for j in range(n)] for i in range(n)]
 print(type(Isempress[0][0]))
-row = []
-
+'''保证每个位置只有0和1的取值, 1为有皇后 2为无皇后'''
+cells_c = [Or(Isempress[i][j] == 0, Isempress[i][j] == 1) for i in range(n) for j in range(n)]
 '''保证同一行只有一个皇后,保证同一列只有一个皇后'''
-for i in range(n):
-    s.add(Or(Isempress[i][j])for j in range(n))
-    s.add(And(Not(And(Isempress[i][j], Isempress[i][k]))) for j in range(n) for k in range(j+1, n))
-    s.add(Or(Isempress[j][i])for j in range(n))
-    s.add(And(Not(And(Isempress[j][i], Isempress[k][i]))) for j in range(n) for k in range(j + 1, n))
-
+rows = [Sum(Isempress[i]) == 1 for i in range(n)]
+cols = [Sum([Isempress[i][j] for i in range(n)]) == 1 for j in range(n)]
 '''保证同对角线只有一个皇后'''
-for i in range(n-1):
-    s.add(And(Not(And(Isempress[i+j][j], Isempress[i+k][k]))) for j in range(n-i) for k in range(j+1, n-i))
-    s.add(And(Not(And(Isempress[j][i+j], Isempress[k][i+k]))) for j in range(n-i) for k in range(j+1, n-i))
-    s.add(And(Not(And(Isempress[i+j][n-j], Isempress[i+k][n-k]))) for j in range(n-i) for k in range(j+1, n-i))
-    s.add(And(Not(And(Isempress[j][n-i-j], Isempress[k][n-i-k]))) for j in range(n-i) for k in range(j+1, n-i))
+diagonals1 = [And(Sum([Isempress[i+k][k] for k in range(n-i)]) <= 1,
+                  Sum([Isempress[k][i+k] for k in range(n-i)]) <= 1) for i in range(n)]
+diagonals2 = [And(Sum([Isempress[k][n-1-i-k] for k in range(n-i)]) <= 1,
+                  Sum([Isempress[i+k][n-1-k] for k in range(n-i)]) <= 1) for i in range(n)]
 
+s.add(cells_c + rows + cols + diagonals1 + diagonals2)
 check = s.check()
 print(check)
+if check == sat:
+    m = s.model()
+    print(m)
+else:
+    print("Failed solve")
